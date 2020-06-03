a7b9127e10d442e51a0955beda2f92b0a99ffb52
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-03 14:23:11 -0700

    add output div and UI logic
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-// Business Logic
+// UI Logic
+$(document).ready(function() {
+  $("form#appointmentInfo").submit(function(event) {
+    event.preventDefault();
+    const name = "Your Name: " + $("#name").val();
+    const date = "Your Date: " + $("#date").val();
+    const description = "Your Description: " + $("#description").val();
+    const startEnd = "Your Start and End: " + $("#start-end").val();
+    $("#output").toggle();
+    $("#yourName").text(name);
+    $("#yourDate").text(date);
+    $("#yourDescription").text(description);
+    $("#yourStartEnd").text(startEnd);
+  });
+});
